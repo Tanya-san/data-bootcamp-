b2b8fcd582861fa9df5ac3e93d66b601d1edacf3
Tanya-san <tanasanti9@gmail.com>
2024-10-28 15:17:55 +0700

    just practicing

---
+++ dashboard/prectice caret.R
@@ -0,0 +1,35 @@
+install.packages("caret")
+library(caret)
+
+data(mtcars)
+
+mtcars$am<- factor(mtcars$am,
+                   levels=c(0,1),
+                   labels=c("Auto","Manual"))
+
+#split data
+train_test_split <- function(data) {
+  set.seed(42)
+  n<-nrow(data)
+  id<- sample(n,size=0.7*n)
+  train_data <- data[id,]
+  test_data <- data[-id,]
+  return(list(train_data, test_data))
+}
+
+split_data <- train_test_split(mtcars)
+
+#train model
+glm_model <- train(am ~ mpg,
+      data= split_data[[1]],
+      method="glm")
+
+#score and evaluate
+p<- predict(glm_model, newdata=split_data[[2]])
+
+acc<- mean(p == split_data[[2]]$am)
+
+
+
+
+
